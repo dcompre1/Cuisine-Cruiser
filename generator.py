8763d245792ab724527f3d3318b9b697854e1c1b
Dinamary Compres <dinmarycompres@gmail.com>
2022-07-01 19:04:54 +0000

    initial commit

---
+++ generator.py
@@ -0,0 +1,25 @@
+# loop:
+# prompt user: vegan or vegetarian?
+# prompt user: main ingredient, restrictions, cuisine type
+
+# if no restrictions: www.themealdb.com/api/json/v1/1/random.php
+
+# else:
+# API get recipes based on MI, if not, get based on vegan or vegetarian
+
+# filter out any that have the RI
+
+# filter out any outside of cuisine type, if possible
+
+# if there are multiple recipes, rand() select one to display
+
+# loop(true)
+# prompt user: we've selected recipe_name for you: would you like to view it?
+# prompt user: press l if you'd like to view a new one or q if you'd like to quit(system exit)
+# if no other recipes: There are no other recipes that meet your criteria, continue
+
+# output:
+# recipe
+# ingredients
+# if exists, youtube video
+
