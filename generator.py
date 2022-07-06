c1cd9a6e1e799614711211607d2beff3d00d51c8
Dinamary Compres <dinmarycompres@gmail.com>
2022-07-06 17:37:30 +0000

    style corrections

--- generator.py
+++ generator.py
@@ -10,9 +10,11 @@ print("Welcome to the Random Recipe Generator!")
 while(True):
 
     # prompt user: input preferences or receive recipe?
-    skip = input("\nTo begin, please indicate whether you would like to input preferences" +
-                 " like ingredients, cuisine type, etc. or receive a recipe now:" +
-                 " \n(y) yes, receive now\n(n) no, I have preferences to enter\n")
+    skip = input("\nTo begin, please indicate whether you would like" +
+                 " to input preferences like ingredients, " +
+                 "cuisine type, etc. or receive a recipe now:" +
+                 " \n(y) yes, receive now\n(n) no, I have" +
+                 " preferences to enter\n")
     if skip == "y":
         # no restrictions
         # make into a loop for input to prompt if they want a different recipe
@@ -25,31 +27,37 @@ while(True):
     # yes restrictions
     # prompt user: vegan or vegetarian?
     diet = input("\nIndicate if you are vegan or vegetarian:\n" +
-                     "press (v) for Vegan\n" +
-                     "press (t) for Vegetarian\n" +
-                     "press any other key to continue: ")
+                 "press (v) for Vegan\n" +
+                 "press (t) for Vegetarian\n" +
+                 "press any other key to continue: ")
     # if not vegan or vegetarian, prompt for main ingredient
     if diet != "v" and diet != "t":
-        main_in = input("\nAre you craving a main ingredient that you would like" +
-                            " to include in your dish? If yes, please enter the name, if not press (n):")
+        main_in = input("\nAre you craving a main ingredient" +
+                        " that you would like" +
+                        " to include in your dish? If yes, please" +
+                        " enter the name, if not press (n):")
         main_in = main_in.replace(" ", "_")
         main_in = main_in.lower()
         # prompt for any restrictions
-        ''' restricts = input("\nDo you have any ingredients that you would like to exclude" +
+        ''' restricts = input("\nDo you have any ingredients" +
+                          "that you would like to exclude" +
                           " from potential meals?\n" +
-                          "Please enter each ingredient with space inbetween, or " +
-                          "type \"none\" if you have no restrictions: ")
+                          "Please enter each ingredient with space" +
+                          " inbetween, or type \"none\"" +
+                          " if you have no restrictions: ")
         restricts = restricts.lower()
         restrictions = restricts.split() '''
         # prompt for cuisine type
         '''yes = True
         while (yes):
             cuisine = input("\nFinally, please indicate a cuisine type,\n" +
-                            "to see a list of possible cuisine types press (y),\n" +
-                            " to skip this step and receive a recipe result press (n):")
+                            "to see a list of possible cuisine types" +
+                            "press (y),\n to skip this step and" +
+                            " receive a recipe result press (n):")
             cuisine = cuisine.lower()
             if cuisine == "y":
-                response = requests.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+                response = requests.get("https://www.themealdb.com" +
+                                        "/api/json/v1/1/list.php?a=list")
                 data = response.json()["meals"]
                 areas = []
                 for datum in data:
@@ -60,19 +68,24 @@ while(True):
 
         # API get recipes based on MI, if not, get based on vegan or vegetarian
     if diet == "v":
-        response = requests.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
+        response = requests.get("https://www.themealdb.com" +
+                                "/api/json/v1/1/filter.php?c=Vegan")
     elif diet == "t":
-        response = requests.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
+        response = requests.get("https://www.themealdb.com" +
+                                "/api/json/v1/1/filter.php?c=Vegetarian")
     elif main_in != "n":
-        response = requests.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + main_in)
+        response = requests.get("https://www.themealdb.com" +
+                                "/api/json/v1/1/filter.php?i=" + main_in)
     else:
-        response = requests.get("https://www.themealdb.com/api/json/v1/1/random.php")
+        response = requests.get("https://www.themealdb.com" +
+                                "/api/json/v1/1/random.php")
 
     meal_data = response.json()["meals"]
     new_data = []
     for i in range(len(meal_data)):
         meal = meal_data[i]
-        resp = requests.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + meal["idMeal"])
+        resp = requests.get("https://www.themealdb.com" +
+                            "/api/json/v1/1/lookup.php?i=" + meal["idMeal"])
         meal = resp.json()["meals"][0]
         # get list of meals that meet the specifications
         '''if has_restrictions(meal, restrictions) is False:
@@ -86,9 +99,11 @@ while(True):
     chosen_meal = random.choice(new_data)
     print_recipe(chosen_meal)
     while True:
-        decision = input("\nIf you would like to view a different recipe press (y)," +
-                             "\nif you would like to begin the search over again press (p),\n" +
-                             "otherwise press (q) to quit\n")
+        decision = input("\nIf you would like to view a different" +
+                         " recipe press (y)," +
+                         "\nif you would like to begin the search" +
+                         " over again press (p),\n" +
+                         "otherwise press (q) to quit\n")
         if decision == "y":
             chosen_meal = random.choice(new_data)
             print_recipe(chosen_meal)
@@ -100,6 +115,9 @@ while(True):
             exit()
             # go = False
 
-# idea for database use: keep a database of meal ids inorder to keep record of user's previously viewed recipes
-# another database idea: every time a recipe is chosen it is added to the database and so
-# we keep a record of the most popular dishes in certain areas, categories
+''' idea for database use: keep a database of meal ids inorder to keep
+    record of user's previously viewed recipes '''
+'''another database idea: every time a recipe is chosen it is
+   added to the database and so we keep a record of the most popular
+   dishes in certain areas, categories '''
+'''another idea: use database to make meal plan'''
