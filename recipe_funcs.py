8bc772dd6ebffe95b6e276e8370cdd8582585120
Dinamary Compres <dinmarycompres@gmail.com>
2022-07-07 21:02:15 +0000

    Revert "more database work"
    This reverts commit a926e17e066d5e2f4d7e185aa3260793d69b81d3.
    
    Co-authored-by: Marianela Luna-Torrado <mlunatorrado@gmail.com>

--- recipe_funcs.py
+++ recipe_funcs.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import requests
 
 def print_recipe(meal):
     '''print meal and recipe details'''
@@ -9,10 +8,9 @@ def print_recipe(meal):
 
     print("Here is your recipe: " + meal_name)
     # print(instructions)
-
-    ingredient = meal['strIngredient1']
-    measure = meal['strMeasure1']
-    i = 2
+    i = 1
+    ingredient = meal['strIngredient' + str(i)]
+    measure = meal['strMeasure' + str(i)]
     while ingredient is not None and ingredient != "":
         print(measure + " " + ingredient)
         ingredient = meal['strIngredient' + str(i)]
@@ -24,33 +22,20 @@ def print_recipe(meal):
 
 def has_restrictions(meal, restrictions):
     '''function to check that meal does not contain users restrictions'''
-    ingredient = meal['strIngredient1']
-    j = 2
-    while ingredient is not None and ingredient != "":
+    i = 1
+    while i < 21:
+        ingredient = meal['strIngredient' + str(i)]
         ingredient = ingredient.lower()
-        for i in range(len(restrictions)):
-            if restrictions[i] in ingredient:
-                return True
-        j += 1
-        ingredient = meal['strIngredient' + str(j)]
+        if ingredient in restrictions:
+            return True
     return False
 
 def filter_meals(engine, restrictions):
     query_results = engine.execute("SELECT idMeal FROM meals").fetchall()
-    num_ids = engine.execute("SELECT COUNT(idMeal) FROM meals").fetchall()[0][0]
-    meals = []
-    for i in range(num_ids):
-        id = pd.DataFrame(query_results)[0][i]
-        result = requests.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id)
-        meal = result.json()["meals"][0]
-        r = has_restrictions(meal, restrictions)
-        # has restrictions, so needs to be removed from database
-        if r == True:
-            engine.execute("DELETE FROM meals WHERE idMeal = " + id)
-        else:
-            meals.append(meal)
-    query_results = engine.execute("SELECT idMeal FROM meals").fetchall()
-    return meals
-    # if any ingredients overlap with those in restrictions, remove meal from database
-    # save meal info of meals that don't have restrictions
-
+    # print(pd.DataFrame(query_result))
+    num_ids = engine.execute("SELECT COUNT(idMeal) FROM meals").fetchall()
+    print(num_ids)
+    # get ids
+    # for each id, make an api call so we can get ingredients
+    # if any ingredients overlap with those in restrictions, remove meal from database 
+    
